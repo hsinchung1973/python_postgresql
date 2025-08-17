95221d991b82d14d83129e04bb57ce85025163df
hsinchung <hsinchung1973@gmail.com>
2025-08-17 05:42:40 +0000

    MOD

--- lesson11/lesson11_1.py
+++ lesson11/lesson11_1.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from ..lesson10 import datasource
+import datasource
 
 st.sidebar.title("台鐵車站資訊")
 st.sidebar.header("2023年各站進出人數")
@@ -11,9 +11,21 @@ def get_stations():
     return datasource.get_stations_names()
 
 stations = get_stations()
-station = st.sidebar.selectbox(
-    "請選擇車站",
-    stations,
-)
+if stations is None:
+    st.error("無法取得車站資料，請稍後再試。")
+    st.stop()
+
+
+common_stations = ['臺北','桃園','新竹','台中','臺南','高雄','其它']
+
+choice = st.sidebar.radio("快速選擇常用車站", common_stations)
+
+if choice == "其它":
+    station = st.sidebar.selectbox(
+        "請選擇車站",
+        stations,
+    )
+else:
+    station = choice
 
 st.write("您選擇的車站:", station)
